e45909f826f16ada7a905446c6e09d5a4fb71dbf
DESKTOP-0V7LJFH\ICNCAST <changwoo1208@naver.com>
2023-08-25 15:30:54 +0900

    topic init

---
+++ lib/page/topic_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class TopicPage extends StatefulWidget {
+  const TopicPage({super.key});
+
+  @override
+  State<TopicPage> createState() => _TopicPageState();
+}
+
+class _TopicPageState extends State<TopicPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
